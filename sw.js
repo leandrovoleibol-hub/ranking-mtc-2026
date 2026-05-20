6190b4ae2c0c2baaaa056d74b08b461503e80000
leandrovoleibol-hub <leandrovoleibol@gmail.com>
2026-05-20 13:55:44 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'mtc-2026-v1';
+const ASSETS = [
+  '/ranking-mtc-2026/',
+  '/ranking-mtc-2026/index.html',
+  '/ranking-mtc-2026/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
